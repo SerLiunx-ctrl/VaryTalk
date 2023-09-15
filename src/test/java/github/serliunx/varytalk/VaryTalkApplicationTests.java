ccf4cb6bda629c3fc65aaac22e7366e1e1c69126
SerLiunx-ctrl <17689543@qq.com>
2023-09-16 00:07:38 +0800

    新增单元测试.

---
+++ src/test/java/github/serliunx/varytalk/VaryTalkApplicationTests.java
@@ -0,0 +1,12 @@
+package github.serliunx.varytalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaryTalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
