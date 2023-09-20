b70965a6cd0f69b0c226bcd9ee05dfcb066003b3
SerLiunx-ctrl <17689543@qq.com>
2023-09-20 17:08:08 +0800

    新增权限缓存以增加访问速度.

--- src/main/java/github/serliunx/varytalk/common/aop/PermissionAdvice.java
+++ src/main/java/github/serliunx/varytalk/common/aop/PermissionAdvice.java
@@ -1,19 +1,23 @@
 package github.serliunx.varytalk.common.aop;
 
 import github.serliunx.varytalk.common.annotation.PermissionRequired;
+import github.serliunx.varytalk.common.config.autoconfiguer.SystemAutoConfigurer;
 import github.serliunx.varytalk.common.exception.ServiceException;
 import github.serliunx.varytalk.common.util.AopUtils;
+import github.serliunx.varytalk.common.util.RedisUtils;
 import github.serliunx.varytalk.common.util.SecurityUtils;
 import github.serliunx.varytalk.project.system.entity.SystemUser;
-import github.serliunx.varytalk.project.system.service.SystemUserPermissionService;
 import github.serliunx.varytalk.project.system.service.SystemUserService;
 import github.serliunx.varytalk.security.service.PermissionService;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Method;
+import java.util.concurrent.TimeUnit;
 
 @Aspect
 @Component
@@ -21,25 +25,41 @@ public class PermissionAdvice {
 
     private final SystemUserService systemUserService;
     private final PermissionService permissionService;
+    private final SystemAutoConfigurer systemAutoConfigurer;
+    private final Logger logger;
+    private final RedisUtils redisUtils;
 
     public PermissionAdvice(SystemUserService systemUserService,
-                            PermissionService permissionService) {
+                            PermissionService permissionService,
+                            SystemAutoConfigurer systemAutoConfigurer,
+                            RedisUtils redisUtils) {
 
         this.systemUserService = systemUserService;
         this.permissionService = permissionService;
+        this.systemAutoConfigurer = systemAutoConfigurer;
+        this.redisUtils = redisUtils;
+
+        logger = LoggerFactory.getLogger(PermissionAdvice.class);
     }
 
     @Before("github.serliunx.varytalk.common.aop.PointCutDefinition.permissionPoint()")
     public void permissionCheck(JoinPoint joinPoint){
         Long userId = SecurityUtils.getUserId();
-        Method method = AopUtils.getMethodByJoinPoint(joinPoint);
-        PermissionRequired annotation = method.getAnnotation(PermissionRequired.class);
-        if(annotation == null){
-            return;
+        String key = systemAutoConfigurer.getRedisPrefix().getJoinPointCache() +
+                joinPoint.getSignature().toString().replace(" ", "-");
+        String permission = (String)redisUtils.get(key);
+        if(permission == null){
+            Method method = AopUtils.getMethodByJoinPoint(joinPoint);
+            PermissionRequired annotation = method.getAnnotation(PermissionRequired.class);
+            if(annotation == null){
+                return;
+            }
+            permission = annotation.value();
+            logger.info("新访问->权限鉴定接口: {} 已缓存. 权限值: {}", joinPoint.getSignature().toString(), permission);
+            redisUtils.put(key, permission, 1, TimeUnit.DAYS);
         }
         SystemUser systemUser = systemUserService.selectUserById(userId);
-        String permissionValue = annotation.value();
-        boolean result = permissionService.hasPermission(systemUser, permissionValue);
+        boolean result = permissionService.hasPermission(systemUser, permission);
         if(!result){
             throw new ServiceException("权限不足, 无法进行此操作!", 400);
         }
