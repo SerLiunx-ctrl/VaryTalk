972cbf5f6acc47cddad29ab66ef936f44c249a43
SerLiunx-ctrl <17689543@qq.com>
2024-02-10 18:57:28 +0800

    重新启用定时任务.

---
+++ varytalk-module-schedule/src/main/java/com/serliunx/varytalk/schedule/config/ScheduleConfiguration.java
@@ -0,0 +1,15 @@
+package com.serliunx.varytalk.schedule.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * 定时任务相关配置信息
+ * @author SerLiunx
+ * @since 1.0
+ */
+@Configuration
+@EnableScheduling
+public class ScheduleConfiguration {
+
+}
---
+++ varytalk-module-schedule/src/main/java/com/serliunx/varytalk/schedule/constant/MessageStorage.java
@@ -0,0 +1,10 @@
+package com.serliunx.varytalk.schedule.constant;
+
+/**
+ * 相关提示信息
+ * @author SerLiunx
+ * @since 1.0
+ */
+public interface MessageStorage {
+    String SCHEDULE_PREFIX = "[定时任务]";
+}
